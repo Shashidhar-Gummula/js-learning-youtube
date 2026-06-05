260c6844e46f490195f2824397a08a1c9b87125b
Shashidhar Gummula <shashidhargummula@gmail.com>
2026-06-05 05:25:09 +0000

    container config done

---
+++ 01 basics/01-variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "shashi@google.com"
+var accountPassword = "12345"
+accountCity = "Bangalore"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "sh@hc.com"
+accountPassword = "221133"
+accountCity = "Mumbai"
+
+console.log("accountId");
+
+/*
+prefer not to use var and use let and const instead of var
+becasue of issue in bock scope and function scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
