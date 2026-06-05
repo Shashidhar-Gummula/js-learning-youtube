1b7fc1311a679ef8a4e2345ca304bedcbc0d86fe
Shashidhar Gummula <shashidhargummula@gmail.com>
2026-06-05 13:14:49 +0000

    datatypes

--- 01 basics/03-conversionOperation.js
+++ 01 basics/03-conversionOperation.js
@@ -24,5 +24,46 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+// ________________________ Operations ________________________
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+// This are operations in JavaScript
+// console.log(3 + 2);     // Addition
+// console.log(3 - 2);     // Subtraction
+// console.log(3 * 2);     // Multiplication
+// console.log(3 / 2);     // Division
+// console.log(3 % 2);     // Modulus (Remainder)
+// console.log(3 ** 2);    // Exponentiation (Power)
+
+let str1 = "hello"
+let str2 = " shashi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3 );
+
+// console.log(+true);    => don't use this type of conversion in code
+// console.log(+"");      => don't use this type of conversion in code
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
