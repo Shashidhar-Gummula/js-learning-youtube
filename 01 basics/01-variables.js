a0171d66f1f0a8b4f223c048dfc291b1a3c14134
Shashidhar Gummula <shashidhargummula@gmail.com>
2026-06-05 13:11:35 +0530

    Update 01-variables.js

--- 01 basics/01-variables.js
+++ 01 basics/01-variables.js
@@ -14,7 +14,7 @@ console.log("accountId");
 
 /*
 prefer not to use var and use let and const instead of var
-becasue of issue in block scope and function scope
+becasue of issue in bock scope and function scope
 */
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
